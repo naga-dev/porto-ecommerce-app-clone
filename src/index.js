a41300da199f7927aa876364d80c15a763540b5b
naga-dev <nagadev93@gmail.com>
2020-10-03 17:14:23 +0200

    Clear app folder structure

--- src/index.js
+++ src/index.js
@@ -10,14 +10,4 @@ const render = () => ReactDOM.render(<App />, rootEl);
 if (module.hot) module.hot.accept('./App.js', () => setTimeout(() => render()));
 render();
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
