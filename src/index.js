541f78ab599383990a72020dd75d5ba2a391d364
naga-dev <nagadev93@gmail.com>
2020-10-03 17:11:43 +0200

    Adding a Hot Module Replacement

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Make a hot module replacement
+const rootEl = document.getElementById("root");
+const render = () => ReactDOM.render(<App />, rootEl);
+
+if (module.hot) module.hot.accept('./App.js', () => setTimeout(() => render()));
+render();
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
