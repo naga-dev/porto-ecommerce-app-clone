fdd64e2c43e274e8c4cdd0bd18129cb21f820675
naga-dev <nagadev93@gmail.com>
2020-10-09 19:18:30 +0200

    Make product modal and style it in test page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import ProductPage from "./components/ProductPage";
-import AppFooter from "./components/AppFooter";
+// import AppFooter from "./components/AppFooter";
 import HomePage from "./pages/HomePage";
 import ShopPage from "./pages/ShopPage";
+import TextPage from "./pages/TextPage";
 import "./scss/App.scss";
 
 function App() {
@@ -13,8 +14,9 @@ function App() {
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
         <Route path="/shop/product" component={ProductPage} />
+        <Route path="/test" component={TextPage} />
       </Switch>
-      <AppFooter />
+      {/* <AppFooter /> */}
     </div>
   );
 }
